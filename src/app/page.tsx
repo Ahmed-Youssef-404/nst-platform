18784640c8ea1a74f95997c72011b26d81fd4e9b
Ahmed Youssef <ahmed.youssef.5522.52@gmail.com>
2026-07-30 14:47:08 +0300

    feat: handle users page if opened  "/"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,30 @@ import { Stats } from "@/components/landing/stats";
 import { Quote } from "@/components/landing/quote";
 import { Contact } from "@/components/landing/contact";
 import { Footer } from "@/components/landing/footer";
+import { getCurrentUser } from "@/lib/auth/get-current-user";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+
+  const user = await getCurrentUser();
+
+  // if (!user) {
+  //   redirect("/login");
+  // }
+
+  if (user?.role === "instructor") {
+    redirect("/instructor");
+  }
+
+  if (user?.role === "student") {
+    redirect("/student");
+  }
+
+  if (user?.role === "super_admin") {
+    redirect("/super_admin");
+  }
+
+
   return (
     <div className="flex min-h-full flex-1 flex-col">
       <Navbar />
