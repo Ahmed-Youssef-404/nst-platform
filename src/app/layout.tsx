95d651b7be728c77840abba13e0b7148c17f17a3
Ahmed Youssef <ahmed.youssef.5522.52@gmail.com>
2026-09-11 03:01:37 +0300

    feat: initialize root layout with the new "CSS" file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import "./nst-design-v1.0.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 
